317b5ee75d4ff6c2057d4dfa8f4ae30ee708d194
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-06-13 14:50:04 +0900

    Change aws auth provider

--- nuxeo-aws-bedrock-connector-core/src/main/java/org/nuxeo/labs/aws/bedrock/service/AWSBedrockServiceImpl.java
+++ nuxeo-aws-bedrock-connector-core/src/main/java/org/nuxeo/labs/aws/bedrock/service/AWSBedrockServiceImpl.java
@@ -2,12 +2,12 @@ package org.nuxeo.labs.aws.bedrock.service;
 
 import org.nuxeo.runtime.model.DefaultComponent;
 
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelRequest;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelResponse;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 
 
 public class AWSBedrockServiceImpl extends DefaultComponent implements AWSBedrockService {
@@ -16,7 +16,7 @@ public class AWSBedrockServiceImpl extends DefaultComponent implements AWSBedroc
 
         try (BedrockRuntimeClient client = BedrockRuntimeClient.builder()
                 .region(Region.US_WEST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build()) {
 
             InvokeModelRequest request = InvokeModelRequest.builder()
