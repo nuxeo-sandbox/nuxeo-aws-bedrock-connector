7fbe3adf8e6367c015f42ef358e8709819b44791
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-12-17 17:56:16 +0900

    Upgrade to Java 21 API

--- nuxeo-aws-bedrock-connector-core/src/test/java/org/nuxeo/labs/aws/bedrock/TestAWSBedrockService.java
+++ nuxeo-aws-bedrock-connector-core/src/test/java/org/nuxeo/labs/aws/bedrock/TestAWSBedrockService.java
@@ -19,7 +19,7 @@ import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelResponse;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
